0bad00e54ae3eba31032cf60dc2a739bbdd80e11
liyazhou <liyazhou@outlook.com>
2017-01-10 11:18:14 +0800

    list tuple string for while

--- samples/basic/the_dict.py
+++ samples/basic/the_dict.py
@@ -10,3 +10,26 @@ print('d[\'Michael\'] =', d['Michael'])
 print('d[\'Bob\'] =', d['Bob'])
 print('d[\'Tracy\'] =', d['Tracy'])
 print('d.get(\'Thomas\', -1) =', d.get('Thomas', -1))
+print('______________')
+
+print(d.get('test'))
+print(d.get('t', -1))
+print(d)
+print(d.pop('Bob'))
+print(d)
+d['t'] = 'liyazhou'
+print(d)
+d.popitem()
+print(d)
+d.popitem()
+print(d)
+d.clear()
+print(d)
+d = {
+    't': 95,
+    'a': 75,
+    'c': 85
+}
+print(d)
+a = d.copy()
+print(a)
--- samples/basic/the_set.py
+++ samples/basic/the_set.py
@@ -6,3 +6,13 @@ print(s1)
 s2 = set([2, 3, 4])
 print(s1 & s2)
 print(s1 | s2)
+s3 = s1 & s2
+print(s3)
+s4 = s1 | s2
+print(s4)
+s1.add('5')
+print(s1)
+s1.add(0.5)
+print(s1)
+s1.remove(3)
+print(s1)
