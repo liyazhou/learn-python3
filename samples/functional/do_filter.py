6997c142060b82da880489c8fd700f41bda1b749
liyazhou <liyazhou@outlook.com>
2017-01-13 11:48:04 +0800

    higher order function filter sorted

--- samples/functional/do_filter.py
+++ samples/functional/do_filter.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 def is_odd(n):
     return n % 2 == 1
+
+
+def is_even(n):
+    return n % 2 == 0
+
 
 L = range(100)
 
 print(list(filter(is_odd, L)))
+print(list(filter(is_even, L)))
+
 
 def not_empty(s):
     return s and s.strip()
 
+
+def not_empty_2(s):
+    return s and s.strip()
+
+
 print(list(filter(not_empty, ['A', '', 'B', None, 'C', '  '])))
+print(list(filter(not_empty_2, ['A', '', 'B', None, 'C', '  '])))
--- samples/functional/do_sorted.py
+++ samples/functional/do_sorted.py
@@ -7,9 +7,12 @@ L = ['bob', 'about', 'Zoo', 'Credit']
 
 print(sorted(L))
 print(sorted(L, key=str.lower))
+print(sorted(L, key=str.lower))
 
 students = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 
 print(sorted(students, key=itemgetter(0)))
+print(sorted(students, key=itemgetter(1)))
+print(sorted(students, key=lambda t: t[1]))
 print(sorted(students, key=lambda t: t[1]))
 print(sorted(students, key=itemgetter(1), reverse=True))
