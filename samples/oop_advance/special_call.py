0c02cf7e48867ab5c012c2edfcdd64b7fd881cf2
liyazhou <liyazhou@outlook.com>
2017-02-06 14:54:46 +0800

    define class str call getattr getitem iter

--- samples/oop_advance/special_call.py
+++ samples/oop_advance/special_call.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 class Student(object):
-    def __init__(self, name):
+    def __init__(self, name='student'):
         self.name = name
 
     def __call__(self):
         print('My name is %s.' % self.name)
 
+
 s = Student('Michael')
 s()
+
+
+class Teacher(object):
+    def __init__(self, name='teacher'):
+        self.name = name
+
+    def __call__(self):
+        print('teacher name is %s.' % self.name)
+
+
+t = Teacher('liyazhou')
+t()
+
+print(callable(Student()))
+print(callable(Teacher()))
+print(callable([1, 2, 3]))
+print(callable('123'))
+print(callable(max))
--- samples/oop_advance/special_getattr.py
+++ samples/oop_advance/special_getattr.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-class Student(object):
 
+class Student(object):
     def __init__(self):
         self.name = 'Michael'
 
     def __getattr__(self, attr):
-        if attr=='score':
+        if attr == 'score':
             return 99
-        if attr=='age':
+        if attr == 'age':
             return lambda: 25
-        raise AttributeError('\'Student\' object has no attribute \'%s\'' % attr)
+            # raise AttributeError('\'Student\' object has no attribute \'%s\'' % attr)
+
 
 s = Student()
 print(s.name)
@@ -19,3 +20,41 @@ print(s.score)
 print(s.age())
 # AttributeError: 'Student' object has no attribute 'grade'
 print(s.grade)
+
+
+class Teacher(object):
+    def __init__(self):
+        self.name = 'Michael'
+
+    def __getattr__(self, attr):
+        if attr == 'score':
+            return 99
+        if attr == 'age':
+            return lambda: 25
+            # error
+            # return 25
+        raise AttributeError('teacher object has no attribute %s' % attr)
+
+
+t = Teacher()
+print(t.name)
+print(t.score)
+# function
+print(t.age())
+print(t.grade)
+
+
+class Chain(object):
+    def __init__(self, path=''):
+        self._path = path
+
+    def __getattr__(self, path):
+        return Chain('%s/%s' % (self._path, path))
+
+    def __str__(self):
+        return self._path
+
+    __repr__ = __str__
+
+
+print(Chain().status.user.timeline.list)
