491ff10dde0031f5b7f5e57e7ba05893175c2e0b
liyazhou <liyazhou@outlook.com>
2017-02-06 11:34:33 +0800

    define class str

--- samples/oop_advance/special_str.py
+++ samples/oop_advance/special_str.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-class Student(object):
 
+class Student(object):
     def __init__(self, name):
         self.name = name
 
@@ -12,3 +12,18 @@ class Student(object):
     __repr__ = __str__
 
 print(Student('Michael'))
+s = Student('lyz')
+print(s)
+
+
+class Teacher(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'Teacher object (name %s)' % self.name
+
+    __repr__ = __str__
+
+
+print(Teacher('liyazhou'))
